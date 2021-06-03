04bb943be85ca098ad1c01d97334ee881831f74f
zhangdongxu <zhangdongxuu@gmail.com>
2021-06-02 23:46:09 -0400

    add score_func and classifier

--- bin/auto_sweep.py
+++ bin/auto_sweep.py
@@ -14,7 +14,7 @@ import subprocess
 from pathlib import Path
 
 
-def config_generation(model_type, path, task, data_format, na_mode, max_length, warmup):
+def config_generation(score_func, classifier, path, task, data_format):
 
     sweep_config = {
         # "controller": {"type": "local"},
@@ -28,40 +28,81 @@ def config_generation(model_type, path, task, data_format, na_mode, max_length, 
             "--data_path=" + path,
             task,
             data_format,
-            "--na_mode="+na_mode,
             "--encoder_type=roberta-large",
-            "--score_func=" + model_type,
+            "--score_func=" + score_func,
+            "--classifier=" + classifier,
             "--train_batch_size=2",
             "--test_batch_size=16",
             "--patience=5",
             "--log_interval=0.25",
             "--epochs=10",
-            "--max_text_length=" + str(max_length),
+            "--max_text_length=512",
+            "--dropout_rate=0.1",
             "--max_grad_norm=1.0",
             "--wandb",
+            "--save_embedding",
         ],
         "method": "bayes",
         "metric": {"goal": "maximize", "name": "Micro F1 dev"},
         "parameters": {
-            "learning_rate": {"distribution": "log_uniform", "min": -16.0, "max": -7.0}, # 1e-7 ~ 1e-3
+            # 1e-7 ~ 1e-3
+            "learning_rate": {"distribution": "log_uniform", "min": -16.0, "max": -7.0},
             "grad_accumulation_steps": {"values": [1, 4, 16, 64]},
-            "dim": { "values": [256, 512, 768, 1024]},
-            "weight_decay":{"distribution": "log_uniform", "min": -18.4, "max": -2.3}, # 1e-8 ~ 1e-1
+            "dim": {"values": [256, 512, 768, 1024]},
+            # 1e-8 ~ 1e-1
+            "weight_decay": {"distribution": "log_uniform", "min": -18.4, "max": -2.3},
         },
     }
-    if na_mode != "0":
-        sweep_config["parameters"]["na_weight"] = {
-            "distribution": "log_uniform",
-            "min": -11.51,
-            "max": 2.3,
-        } # 1e-5 ~ 10
-        sweep_config["parameters"]["categorical_weight"] = {
-            "distribution": "log_uniform",
-            "min": -11.51,
-            "max": 2.3,
-        } # 1e-5 ~ 10
 
-    if model_type == "box":
+    if task == "--multi_class":
+
+        if score_func != "cosine" and classifier == "softmax_margin":
+            sweep_config["parameters"]["margin"] = {
+                "distribution": "uniform",
+                "min": 0.0,
+                "max": 10.0}
+
+        elif score_func != "cosine" and classifier in ["pairwise_margin", "pairwise_margin_na"]:
+            sweep_config["parameters"]["margin_pos"] = {
+                "distribution": "uniform",
+                "min": 0.0,
+                "max": 10.0}
+            sweep_config["parameters"]["margin_neg"] = {
+                "distribution": "uniform",
+                "min": 0.0,
+                "max": 10.0}
+
+        elif score_func == "cosine" and classifier == "softmax":
+            sweep_config["parameters"]["rescale_factor"] = {
+                "distribution": "uniform",
+                "min": 1,
+                "max": 200}
+
+        elif score_func == "cosine" and classifier == "softmax_margin":
+            sweep_config["parameters"]["rescale_factor"] = {
+                "distribution": "uniform",
+                "min": 1,
+                "max": 200}
+            sweep_config["parameters"]["margin"] = {
+                "distribution": "uniform",
+                "min": 0.0,
+                "max": 0.5}
+
+        elif score_func == "cosine" and classifier in ["pairwise_margin", "pairwise_margin_na"]:
+            sweep_config["parameters"]["rescale_factor"] = {
+                "distribution": "uniform",
+                "min": 1,
+                "max": 200}
+            sweep_config["parameters"]["margin_pos"] = {
+                "distribution": "uniform",
+                "min": 0.0,
+                "max": 0.5}
+            sweep_config["parameters"]["margin_neg"] = {
+                "distribution": "uniform",
+                "min": 0.0,
+                "max": 0.5}
+
+    if score_func == "box":
         sweep_config["parameters"]["intersection_temp"] = {
             "distribution": "log_uniform",
             "min": -9.2,
@@ -72,39 +113,29 @@ def config_generation(model_type, path, task, data_format, na_mode, max_length, 
             "min": -2.3,
             "max": 2.3,
         }
-        if na_mode in ["3", "4"]:
-            sweep_config["parameters"]["na_box_weight"] = {
-                "distribution": "log_uniform",
-                "min": -11.51,
-                "max": 2.3,
-            } # 1e-5 ~ 10
-    if warmup == True:
-        sweep_config["parameters"]["warmup"] = {
-            "distribution": "uniform", 
-            "min": 0.0, 
-            "max": 0.5}
 
     return sweep_config
-
 
 
 def main(config):
     if config.multi_label:
         task = "--multi_label"
-    else: 
+    else:
         task = "--multi_class"
     if config.full_annotation:
         data_format = "--full_annotation"
     else:
         data_format = "--partial_annotation"
-    sweep_config = config_generation(model_type=config.model_type, path=config.data_path, task=task, data_format=data_format, na_mode=config.na_mode, max_length=config.max_length, warmup=config.warmup)
+    sweep_config = config_generation(score_func=config.score_func, classifier=config.classifier,
+                                     path=config.data_path, task=task, data_format=data_format)
     sweep_id = wandb.sweep(sweep_config, project="re")
     os.system(f"sh bin/launch_train_sweep.sh dongxu/re/{sweep_id} {config.partition} {config.max_run} {config.num_machine-1} {config.memory_per_run}")
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Project: Relation extraction")
+    parser = argparse.ArgumentParser(
+        description="Project: Relation extraction")
     parser.add_argument("--partition", type=str, default="1080ti-long")
     parser.add_argument("--username", type=str, default="dongxuzhang")
     parser.add_argument("--max_run", type=int, default=50)
@@ -112,27 +143,26 @@ if __name__ == "__main__":
     parser.add_argument("--memory_per_run", type=int, default=30000)
 
     parser.add_argument("--data_path", type=str, default="data/tacred/")
-    parser.add_argument("--model_type", type=str, default="box")
     parser.add_argument("--multi_label", action='store_true', default=False)
-    parser.add_argument("--full_annotation", action='store_true', default=False)
-    parser.add_argument("--na_mode", type=str, default="0")
-    parser.add_argument("--max_length", type=int, default=512)
-    parser.add_argument("--warmup", action='store_true', default=False)
+    parser.add_argument("--full_annotation",
+                        action='store_true', default=False)
+    parser.add_argument("--score_func", type=str, default="dot")
+    parser.add_argument("--classifier", type=str, default="softmax")
     config = parser.parse_args()
-    if config.model_type not in [
+    if config.score_func not in [
         "box",
-        "nn",
+        "dot",
+        "cosine",
         "biaffine"
     ]:
-        raise Exception(f"model type {config.model_type} does not exist")
+        raise Exception(f"score_func {config.score_func} does not exist")
 
-    if config.na_mode not in [
-        "0",
-        "1",
-        "2",
-        "3",
-        "4",
+    if config.classifier not in [
+        "softmax",
+        "softmax_margin",
+        "pairwise_margin",
+        "pairwise_margin_na"
     ]:
-        raise Exception(f"na_mode{config.na_mode} does not exist")
+        raise Exception(f"classifier {config.classifier} does not exist")
 
     main(config)
